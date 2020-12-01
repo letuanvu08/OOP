803c442e3b7fde3a504bd99d40c6543bc6105b58
letuanvu08 <vu.le08@hcmut.edu.vn>
2020-12-01 15:04:23 +0700

    merge

--- WindowsFormsApp/WindowsFormsApp/FormRentContract.Designer.cs
+++ WindowsFormsApp/WindowsFormsApp/FormRentContract.Designer.cs
@@ -530,7 +530,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(592, 487);
+            this.ClientSize = new System.Drawing.Size(592, 655);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Confirm);
